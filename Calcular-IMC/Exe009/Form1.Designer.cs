b07ccd79216dee55892e6c710dcbe429b33cc399
gigidborges <giovanaborges1903@gmail.com>
2024-12-15 22:55:35 -0300

    Add files via upload

---
+++ Calcular-IMC/Exe009/Form1.Designer.cs
@@ -0,0 +1,221 @@
+﻿namespace Exe009
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt2 = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.lblImc = new System.Windows.Forms.Label();
+            this.lblClass = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(801, 100);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CALCULO DO ÍNDICE DA MASSA CORPORAL - IMC\r\n";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 132);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(205, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite o Peso:";
+            // 
+            // txt1
+            // 
+            this.txt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt1.Location = new System.Drawing.Point(192, 132);
+            this.txt1.Name = "txt1";
+            this.txt1.Size = new System.Drawing.Size(100, 26);
+            this.txt1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(427, 132);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(148, 26);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Digite a altura:";
+            // 
+            // txt2
+            // 
+            this.txt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt2.Location = new System.Drawing.Point(582, 132);
+            this.txt2.Name = "txt2";
+            this.txt2.Size = new System.Drawing.Size(100, 26);
+            this.txt2.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.BackColor = System.Drawing.Color.DarkViolet;
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Location = new System.Drawing.Point(283, 202);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(161, 54);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "CALCULAR IMC";
+            this.btnCalcular.UseVisualStyleBackColor = false;
+            this.btnCalcular.Click += new System.EventHandler(this.BtnCalcular_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.BackColor = System.Drawing.Color.MediumPurple;
+            this.groupBox1.Controls.Add(this.lblClass);
+            this.groupBox1.Controls.Add(this.lblImc);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(27, 283);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(748, 141);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Resultado";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(96, 47);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(105, 20);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Valor do IMC:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(100, 97);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(106, 20);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "Classificação:";
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.BackColor = System.Drawing.Color.Violet;
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Location = new System.Drawing.Point(83, 480);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(162, 59);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "LIMPAR";
+            this.btnLimpar.UseVisualStyleBackColor = false;
+            this.btnLimpar.Click += new System.EventHandler(this.BtnLimpar_Click);
+            // 
+            // btnSair
+            // 
+            this.btnSair.BackColor = System.Drawing.Color.HotPink;
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Location = new System.Drawing.Point(500, 480);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(157, 59);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "SAIR";
+            this.btnSair.UseVisualStyleBackColor = false;
+            this.btnSair.Click += new System.EventHandler(this.BtnSair_Click);
+            // 
+            // lblImc
+            // 
+            this.lblImc.BackColor = System.Drawing.Color.Khaki;
+            this.lblImc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblImc.Location = new System.Drawing.Point(256, 47);
+            this.lblImc.Name = "lblImc";
+            this.lblImc.Size = new System.Drawing.Size(122, 31);
+            this.lblImc.TabIndex = 4;
+            // 
+            // lblClass
+            // 
+            this.lblClass.BackColor = System.Drawing.Color.Khaki;
+            this.lblClass.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblClass.Location = new System.Drawing.Point(256, 97);
+            this.lblClass.Name = "lblClass";
+            this.lblClass.Size = new System.Drawing.Size(258, 30);
+            this.lblClass.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 587);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txt2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.txt1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt2;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.Label lblClass;
+        private System.Windows.Forms.Label lblImc;
+    }
+}
+
---
+++ Calcular-IMC/Exe009/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Exe009
+{
+    public partial class Form1 : Form
+    {
+        double peso = 0;
+        double altura = 0;
+        double imc = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnLimpar_Click(object sender, EventArgs e)
+        {
+            txt1.Text = "";
+            txt2.Text = "";
+            lblImc.Text = "";
+            lblClass.Text = "";
+
+        }
+
+        private void BtnSair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnCalcular_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                peso = Double.Parse(txt1.Text);
+                altura = Double.Parse(txt2.Text);
+
+                if(peso < 30 || altura < 1 || altura > 3)
+                {
+                    throw new Exception("peso ou altura fora dos limites!!");
+                }
+
+                imc = peso / (altura * altura);
+                lblImc.Text = imc.ToString("00.00");
+
+
+                if (imc < 16)
+                {
+                    lblClass.Text = "Magreza grau 3";
+                }
+
+                else if (imc < 16.0 || imc < 16.9)
+                {
+                    lblClass.Text = "Magreza grau 2";
+                }
+
+                else if (imc < 17.0 || imc < 18.4)
+                {
+                    lblClass.Text = "Magreza grau 1";
+                }
+
+                else if (imc < 18.5 || imc < 24.9)
+                {
+                    lblClass.Text = "Adequado";
+                }
+
+                else if (imc < 25.0 || imc < 29.9)
+                {
+                    lblClass.Text = "Pré-obeso";
+                }
+
+                else if (imc < 30.0 || imc < 34.9)
+                {
+                    lblClass.Text = "Obesidade grau 1";
+                }
+
+                else if (imc < 35.0 || imc < 39.9)
+                {
+                    lblClass.Text = "Obesidade grau 2";
+                }
+
+                else if (imc >= 40)
+                {
+                    lblClass.Text = "Obesidade grau 3";
+                }
+            }
+
+            catch(Exception erro)
+            {
+                MessageBox.Show(erro.Message, "Arenção");
+                txt1.Focus();
+            }
+           
+        }
+    }
+}
